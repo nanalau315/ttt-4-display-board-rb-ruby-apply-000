d55125aadf993d03018bf02f887390c6c2ac8153
Chamaeleonidae Na <nanalau315@hotmail.com>
2020-10-10 17:07:51 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,10 +7,4 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
-" #{board[0]} | #{board[1]} | #{board[2]} "
-"-----------"
-" #{board[3]} | #{board[4]} | #{board[5]} "
-"-----------"
-" #{board[6]} | #{board[7]} | #{board[8]} "
-
 end
